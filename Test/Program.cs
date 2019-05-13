2c6ca21fd1529d28974910c7f881ba72c5b00a15
Costath <thalesdaniel@gmail.com>
2019-05-13 11:34:11 -0400

    Practice tests from VanHack using https://www.hackerrank.com

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Test
 {
@@ -10,6 +18,29 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            List<int> arr = new List<int> { 1, 1, 1};
+            
+            Console.WriteLine("Result = " + SubarraySum(arr));
+        }
+
+        public static long SubarraySum(List<int> arr)
+        {
+            List<int> aux = new List<int> { };
+            aux.AddRange(arr);
+            long result = 0;
+            do
+            {
+                do
+                {
+                    result += aux.Sum();
+                    aux.RemoveAt(aux.Count-1);
+                } while (aux.Count > 0);
+                
+                arr.RemoveAt(0);
+                aux.AddRange(arr);
+            } while (arr.Count > 0);
+
+        return result;
         }
     }
 }
---
+++ Test2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'getElements' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<int> getElements(List<int> arr, List<List<int>> queries)
+    {
+        int sizeArr = arr.Count;
+        int rows = (sizeArr - 1) / arr[0];
+        int columns = arr[0];
+        int[,] matrix = new int[rows, columns];
+        int count = 1;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix[i, j] = arr.ElementAt(count++);
+            }
+        }
+
+        int x, y;
+        List<int> answer = new List<int> { };
+        int queriesQty = queries.Count;
+        for (int i = 0; i < queriesQty; i++)
+        {
+            x = queries[i][0] - 1;
+            y = queries[i][1] - 1;
+            answer.Add(matrix[x, y]);
+        }
+
+        return answer;
+    }
+
+}
